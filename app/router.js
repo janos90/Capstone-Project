c46d52081336fffd688e0154f4003a64a7b51d85
juan <thekingliveson@gmail.com>
2020-09-19 10:19:39 +1200

    app initialization

---
+++ app/router.js
@@ -0,0 +1,14 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'ember-quickstart/config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
+
+Router.map(function() {
+  this.route('login');
+  this.route('jobs');
+  this.route('form');
+  this.route('settings');
+});
---
+++ app/routes/jobs.js
@@ -0,0 +1,27 @@
+import Route from '@ember/routing/route';
+
+export default class JobsRoute extends Route {
+  model() {
+    return [
+      {
+        id: 1,
+        jobNumber: 1,
+        address: "42 wallaby way, sidney, australia",
+        createdBy: 11,
+        createdAt: new Date(),
+        entity: 21,
+        files: [],
+        form: 31
+      }, {
+        id: 2,
+        jobNumber: 2,
+        address: "62 wallaby way, sidney, australia",
+        createdBy: 12,
+        createdAt: new Date(),
+        entity: 22,
+        files: [],
+        form: 32
+      }
+    ]
+  }
+}
